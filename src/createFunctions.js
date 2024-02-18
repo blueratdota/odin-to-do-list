4e78b80d4c28341eda23f62c2a3a762413f0ca2c
blueratdota <jrafaelgocong@gmail.com>
2024-02-18 12:27:41 +0800

    added add function

--- src/createFunctions.js
+++ src/createFunctions.js
@@ -1,5 +1,7 @@
 import {format} from "date-fns"
 import { el } from "date-fns/locale";
+import editIcon from './file-edit-outline.svg'
+import deleteIcon from './trash-can-outline.svg'
 
 class sampleFunction{
     logger(){
@@ -11,36 +13,118 @@ function thisLogs(){
     console.log('this logs');
 }
 
+const entryReference = []
 
-
-class createEntry{
+class toDoEntry{
     constructor(title,details,dueDate,priority){
         this.title = title
         this.details = details
         this.dueDate = dueDate
         this.priority = priority
+        this.displayStatus
+        this.project
+    }
+}
+
+class entryFolder{
+    constructor(){
+        this.folderContent = []
     }
 
-    constructEntry(div){
-        const createEntryDiv = document.createElement("entry")
+    addEntry(theArray){
+        this.folderContent.push(theArray);
+    }
 
+    loopThroughData(dataArray){
+        for (let i in dataArray){
+            const entryData = dataArray[i]
+            if(entryData.displayStatus!="display"){
+                console.log(`${entryData.title} is displayed`);
+                this.constructEntry(entryData)
+                entryData.displayStatus = "display"
+            }
+            else{
+                console.log(`${entryData.title} is already displayed`);
+            }
+        }
+
+    }
+
+    refreshContent(){
+
+    }
+
+    priorityColor(priority){
+        const prioColor = {'low':'green','medium':"yellow",'high':'red'}
+        return prioColor[priority]
+    }
+
+    constructEntry(data){
+        const content = document.querySelector(".content")
+
+        const createEntryDiv = document.createElement("div")
+        createEntryDiv.classList.add('entry')
+
+        // prio container div
         const createPrioContainer = document.createElement("div")
         createPrioContainer.classList.add("priority-container")
-
         const makePrioCircle = document.createElement("div")
         makePrioCircle.classList.add("priority")
-        makePrioCircle.style.backgroundColor = "blue"
+        makePrioCircle.style.backgroundColor = this.priorityColor(data.priority) //output
+
         createPrioContainer.appendChild(makePrioCircle)
 
+        // text container div
+        const createTextContainer = document.createElement('div')
+        createTextContainer.classList.add('text-container')
+        const checkBox = document.createElement('input')
+        checkBox.name = 'done'
+        checkBox.classList.add('done-status')
+        checkBox.type = "checkbox"
+        const titleText = document.createElement('p')
+        titleText.classList.add('text')
+        titleText.textContent = data.title //output
+
+        createTextContainer.appendChild(checkBox)
+        createTextContainer.appendChild(titleText)
+
+        // buttons area
+        const createButtons = document.createElement('div')
+        createButtons.classList.add('buttons')
+        const date = document.createElement('p')
+        date.classList.add('date')
+        date.textContent = format(data.dueDate, "MM/dd/yyyy") //output
+        const editBtn = document.createElement('img')
+        editBtn.classList.add('svg-icon','edit')
+        editBtn.src = editIcon
+        const deleteBtn = document.createElement('img')
+        deleteBtn.classList.add('svg-icon','delete')
+        deleteBtn.src = deleteIcon
+
+        //testing functionallity
+        editBtn.addEventListener('click',function(e){
+            console.log(`${data.title} is the title`);
+        })
+
+        createButtons.appendChild(date)
+        createButtons.appendChild(editBtn)
+        createButtons.appendChild(deleteBtn)
         
-
-
-
-
-        // div.appendChild(createPrioContainer)
-    
+        createEntryDiv.appendChild(createPrioContainer)
+        createEntryDiv.appendChild(createTextContainer)
+        createEntryDiv.appendChild(createButtons)
+        
+        return content.appendChild(createEntryDiv)
     }
+    // testFunction(){
+    //     console.log('test function');
+    //     console.log(this.priorityColor(this.priority));
+    // }
 
 }
 
-export {sampleFunction,createEntry}
+
+export {
+    sampleFunction,
+    toDoEntry,
+    entryFolder}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './style.css'
-import {sampleFunction,createEntry} from "./createFunctions"
+import {sampleFunction,
+    toDoEntry,
+    entryFolder} from "./createFunctions"
 
 console.log('original index.js');
-
 const testNew = new sampleFunction()
-
 testNew.logger()
 
 
@@ -15,11 +15,110 @@ testNew.logger()
 //UI STUFF
 const content = document.querySelector(".content")
 
+// NAV SIDE
+const navAll = document.querySelector("#all")
+const navToday = document.querySelector(".Today")
+const navWeek = document.querySelector(".Week")
+
+// MODAL
+const addModal = document.querySelector(".add-modal")
+const overlay = document.querySelector(".overlay")
+const closeModal = document.querySelector("#close-modal")
+const modalCurrentPage = document.querySelector("#modal-current-page")
+const finalizeEntry = document.querySelector("#finalize-entry")
+const inputRadio = document.querySelectorAll(".input-radio")
+
+const titleModal = document.querySelector("#title")
+const detailsModal = document.querySelector("#details")
+const dateModal = document.querySelector("#date")
+
+
+// ADDING STUFF
+const addEntry = document.querySelector("#add-entry")
+const addOnCurrentPage = document.querySelector("#curret-page")
+
+
+// add to array
+const newEntry1 = new toDoEntry('sample 1','hidden details',new Date(2025, 1, 11),'low')
+const newEntry2 = new toDoEntry('sample 2','hidden details',new Date(2024, 10, 10),'medium')
+const newEntry3 = new toDoEntry('sample 3','hidden details',new Date(2023, 1, 15),'high')
+
+const startPage = (function(){
+    const currentPage = 'All'
+    modalCurrentPage.textContent = currentPage
+    addOnCurrentPage.textContent = currentPage
+})()
+
+
+const generateContent = (function(){
+    console.log('=== generate content ===');
+    const theFolder = new entryFolder()
+    theFolder.addEntry(newEntry1)
+    theFolder.addEntry(newEntry2)
+    theFolder.addEntry(newEntry3)
+
+    theFolder.loopThroughData(theFolder.folderContent)
+    theFolder.loopThroughData(theFolder.folderContent)
+
+    console.log(theFolder.folderContent);
+
+    return{theFolder}
+
+})()
+
+addEntry.addEventListener('click',function(){
+    console.log('check modal page');
+    overlay.classList.remove("hidden")
+    addModal.classList.remove("hidden")
+})
+
+closeModal.addEventListener('click',function(){
+    overlay.classList.add("hidden")
+    addModal.classList.add("hidden")
+})
+
+finalizeEntry.addEventListener('click',function(){
+    const _title = titleModal.value
+    const _details = detailsModal.value
+    const _date = dateModal.value.split("-")
+    // console.log(inputRadio[0]);
+    // console.log(inputRadio[1]);
+    // console.log(inputRadio[2]);
+    const _radio = (function(){
+        for(let i=0; i<3; i++){
+            if(inputRadio[i].checked){
+                return inputRadio[i].value
+            }
+        }      
+    })()
+
+    // console.log(`${_title} === ${_details} === ${_date} === ${_radio}`);
+    const newEntry = new toDoEntry(_title,_details,new Date(_date[0], _date[1],_date[2]),_radio)
+
+    generateContent.theFolder.addEntry(newEntry)
+    generateContent.theFolder.loopThroughData(generateContent.theFolder.folderContent)
+    
+    console.log(generateContent.theFolder.folderContent);
+
+    //function that clears entrey fields
+    //add execute only if all fields have entry
+})
 
 
 
-const newEntry = new createEntry()
-newEntry.constructEntry(content)
+
+
+
+
+
+
+
+
+
+
+// newEntry1.constructEntry(content)
+// newEntry2.constructEntry(content)
+// newEntry3.constructEntry(content)
 
 
 
